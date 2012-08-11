8d86347cab56a0d00f13f6b233e7ba8e811b0afe
Jukka Svahn <web@rahinaa.biz>
2012-08-11 12:19:29 +0300

    Dropped requirements note from the source.

--- rah_comment_spam.php
+++ rah_comment_spam.php
@@ -8,8 +8,6 @@
  * @license GNU GPLv2
  * @link http://rahforum.biz/plugins/rah_comment_spam
  *
- * Requires Textpattern v4.2.0 or newer, and PHP5 or newer
- *
  * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
