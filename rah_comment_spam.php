3107dfa0f26b7dabc470a7879a1f1df829cb3e5e
Jukka Svahn <web@rahinaa.biz>
2014-03-28 15:07:36 +0200

    Updated header comments.

--- rah_comment_spam.php
+++ rah_comment_spam.php
@@ -1,16 +1,30 @@
 <?php
 
+/*
+ * rah_comment_spam - Anti-spam plugin for Textpattern CMS
+ * https://github.com/gocom/rah_comment_spam
+ *
+ * Copyright (C) 2014 Jukka Svahn
+ *
+ * This file is part of rah_comment_spam.
+ *
+ * rah_comment_spam is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_comment_spam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_comment_spam. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /**
- * Rah_comment_spam plugin for Textpattern CMS
+ * The plugin main class.
  *
- * @author  Jukka Svahn
- * @date    2008-
- * @license GNU GPLv2
- * @link    http://rahforum.biz/plugins/rah_comment_spam
- *
- * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU Genral Public License version 2
- * http://www.gnu.org/licenses/gpl-2.0.html
+ * @internal
  */
 
 class Rah_Comment_Spam
